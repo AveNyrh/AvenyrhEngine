0e700a168dcba80a4c9ca9e66ae7a44683cd3b2a
Axel <axeldeconti@gmail.com>
2021-10-24 22:41:07 +0200

    Added typed data

--- src/avenyrh/engine/SceneSerializer.hx
+++ src/avenyrh/engine/SceneSerializer.hx
@@ -1,8 +1,9 @@
 package avenyrh.engine;
 
 using Lambda;
+import avenyrh.gameObject.Component;
+import avenyrh.gameObject.GameObject;
 import avenyrh.utils.JsonUtils;
-import sys.io.FileInput;
 import haxe.ds.StringMap;
 import sys.io.FileOutput;
 import sys.io.File;
@@ -15,35 +16,36 @@ class SceneSerializer
 
     static inline var lineBreak : String = "\n";
 
+    static inline var underscore : String = "_";
+
     public static var path : String = "examples/res/scenes/";
 
-    static var fi : FileInput;
-    
+    static var map : StringMap<Dynamic>;
+
     //-------------------------------
     //#region Public static API
     //-------------------------------
     public static function serialize(scene : Scene) @:privateAccess
     {
-        
         var data : StringMap<Dynamic> = new StringMap();
         var rtti : haxe.rtti.CType.Classdef = haxe.rtti.Rtti.getRtti(Type.getClass(scene));
 
-        var currentObject : StringMap<Dynamic> = new StringMap();
+        map = new StringMap();
 
         //Scene
-        currentObject.set("Name", scene.name);
-        currentObject.set("Class path", getClassPath(scene));
-        currentObject.set("uID", scene.uID.toString());
+        addValue("Name", scene.name);
+        addValue("Class path", getClassPath(scene));
+        addValue("uID", scene.uID.toString());
 
         for(f in rtti.fields)
         {
             if((f.isPublic || f.meta.exists(m -> m.name == "serializable")) && f.type.getName() != "CFunction")
             {
-                currentObject.set(f.name, Reflect.getProperty(scene, f.name));
+                addValue(f.name, Reflect.getProperty(scene, f.name));
             }
         }
 
-        data.set("Scene", currentObject);
+        data.set("Scene", map);
 
         //Add gameobjects, components ...
 
@@ -64,52 +66,21 @@ class SceneSerializer
         var dyn : haxe.DynamicAccess<Dynamic> = haxe.Json.parse(s);
         var data : StringMap<haxe.DynamicAccess<Dynamic>> = JsonUtils.parseToStringMap(dyn);
 
+        //Retrieve scene specific data
         var sceneData : StringMap<Dynamic> = JsonUtils.parseToStringMap(data.get("Scene"));
 
-        var c = Type.resolveClass(sceneData.get("Class path"));
+        //Build a class from string
+        var c = Type.resolveClass(sceneData.get("s_Class path"));
+        var instance : Class<Dynamic> = Type.createInstance(c, [sceneData.get("s_Name")]);
 
-        //To build a class from string
-        var instance : Class<Dynamic> = Type.createInstance(c, [sceneData.get("Name")]);
-        var rtti : haxe.rtti.CType.Classdef = haxe.rtti.Rtti.getRtti(Type.getClass(instance));
-        var fields : Array<String> = Type.getClassFields(instance);
+        //Set scene data
+        setInstanceFields(instance, sceneData);
 
-        for(f in rtti.fields)
-        {
-            if((f.isPublic || f.meta.exists(m -> m.name == "serializable")) && f.type.getName() != "CFunction" && sceneData.exists(f.name))
-            {
-                var n : String = f.name;
-                switch (f.type)
-                {
-                    case CAbstract("Float", []) : //Float
-                        Reflect.setField(instance, f.name, sceneData.get(f.name));
-
-                    case CAbstract("Int", []) : //Int
-                        var sd = sceneData.get(f.name);
-                        Reflect.setField(instance, f.name, sceneData.get(f.name));
-
-                    case CAbstract("Bool", []) : //Bool
-                        Reflect.setField(instance, f.name, sceneData.get(f.name) == "true");
-
-                    case CClass("String", []) : //String
-                        Reflect.setField(instance, f.name, sceneData.get(f.name));
-
-                    case CEnum(_, []) : //Enum
-                        var ev : EnumValue = cast Reflect.getProperty(instance, f.name);
-                        var e : Enum<Dynamic> = Type.getEnum(ev);
-                        Reflect.setField(instance, f.name, Type.createEnumIndex(e, ev.getIndex()));
-
-                    case CClass("GameObject", []) : //GameObject
-                        trace('GameObject ${f.name}');
-    
-                    case _:
-                        trace('Not supported deserialization for ${f.name}');
-                }
-            }
-        }
+        //Add GameObject, Components ...
 
         SceneManager.addScene(cast instance);
 
-        trace('${sceneData.get("Name")} deserialized');
+        trace('${sceneData.get("s_Name")} deserialized');
 
         return false;
     }
@@ -124,6 +95,94 @@ class SceneSerializer
         {
 			case _:
 				throw 'Unknown object type $name = $object (' + Type.typeof(object) + ')';
+        }
+    }
+
+    /**
+     * Prefixes :
+     * a : array
+     * b : bool
+     * c : component
+     * e : enum
+     * f : float
+     * g : gameObject
+     * i : int
+     * s : string
+     */
+    static function addValue(name : String, value : Dynamic) @:privateAccess
+    {
+        switch Type.typeof(value) 
+        {
+            case TInt :
+                map.set('i_$name', value);
+
+            case TBool :
+                map.set('b_$name', value);
+
+            case TFloat :
+                map.set('f_$name', value);
+
+            case TClass(String) :
+                map.set('s_$name', value);
+
+            case TClass(GameObject) :
+                var go : GameObject = cast value;
+                map.set('g_$name', go.uID.toString());
+
+            case TClass(Component) :
+                var comp : Component = cast value;
+                map.set('c_$name', comp.uID.toString());
+
+            case TClass(Array) :
+                map.set('a_$name', value);
+
+            case TEnum(e) :
+                map.set('e_$name', value);
+
+            //TO DO : Maps
+
+            case _:
+                trace('Unknown value type $name = $value (' + Type.typeof(value) + ')');
+        }
+    }
+
+    static function setInstanceFields(inst : Dynamic, dataMap : StringMap<Dynamic>)
+    {
+        var fields : Array<String> = Type.getClassFields(inst);
+
+        for(key => value in dataMap)
+        {
+            if(!fields.contains(key))
+                continue;
+
+            var arr : Array<String> = key.split(underscore);
+            var type : String = arr[0];
+            var fieldName : String = arr[1];
+
+            trace ('$type - $fieldName = $value');
+
+            switch (type)
+            {
+                case "f", "i", "s" : //float, int, string
+                    Reflect.setField(inst, fieldName, dataMap.get(fieldName));
+
+                case "b" : //Bool
+                    Reflect.setField(inst, fieldName, dataMap.get(fieldName) == "true");
+
+                case "e": //Enum
+                    var ev : EnumValue = cast Reflect.getProperty(inst, fieldName);
+                    var e : Enum<Dynamic> = Type.getEnum(ev);
+                    Reflect.setField(inst, fieldName, Type.createEnumIndex(e, ev.getIndex()));
+
+                case "g" : //GameObject
+                    trace('GameObject ${fieldName}');
+
+                case "c" : //Component
+                    trace('GameObject ${fieldName}');
+
+                case _:
+                    trace('Not supported deserialization for ${fieldName}');
+            }
         }
     }
 
@@ -151,37 +210,5 @@ class SceneSerializer
         }
         return path;
     }
-
-    static function getNameAndData(str : String) : NameAndData
-    {
-        var len : Int = str.length;
-        var s : String = "";
-        var i : Int = 0;
-        var ichar : String = "";
-
-        while (true)
-        {
-            if(i >= len)
-                break;
-
-            ichar = str.charAt(i);
-
-            if(ichar == ".")
-                ichar = "/";
-            else if (ichar == "$")
-                ichar = "";
-
-            s += ichar;
-            i++;
-        }
-
-        return {name : "", data : ""};
-    }
     //#endregion
-}
-
-typedef NameAndData =
-{
-    var name : String;
-    var data : Dynamic;
 }
